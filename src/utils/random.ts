42b8e0c3f569bec2d5e64c8c53e46ffdeebdef69
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-08-18 02:25:53 -0400

    Add weighted element

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -3,6 +3,20 @@ export function randomElement<T>(array: readonly T[]) {
     return array[index];
 }
 
+export function weightedRandomElement<T>(array: readonly [number, T][]) {
+    const total = array.reduce((a, b) => a + b[0], 0);
+    const weight = Math.random() * total;
+
+    let runningTotal = 0;
+    for (const item of array) {
+        runningTotal += item[0];
+        if (runningTotal > weight) {
+            return item;
+        }
+    }
+    throw new Error("This code should be unreachable");
+}
+
 export function chance(probability: number) {
     return Math.random() < probability;
 }
