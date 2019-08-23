d253c11bc5f2ec32d7c6fab3b2652b98f33582e8
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-08-23 00:07:21 -0400

    Add actions

--- src/message-handlers/regular-interactions/decorative/sentence-generator/parts-of-speech.ts
+++ src/message-handlers/regular-interactions/decorative/sentence-generator/parts-of-speech.ts
@@ -7,7 +7,8 @@ const indefinite = indefiniteUntyped as unknown as (string: string) => string;
 export function randomSentence() {
     return capitalizeFirstLetter(`${randomElement([
         description,
-        action,
+        subjectlessAction,
+        subjectedAction,
     ])()}.`);
 }
 
@@ -177,6 +178,171 @@ const verbs = {
                 ]);
             },
         },
+        plural: {
+            anyTense() {
+                return randomElement([
+                    verbs.subjectless.plural.past,
+                    verbs.subjectless.plural.present,
+                    verbs.subjectless.plural.future,
+                ])();
+            },
+            present() {
+                return randomElement([
+                    "run",
+                    "die",
+                    "fall",
+                    "lie",
+                    "speak",
+                    "think",
+                    "talk",
+                    "beep",
+                    "sing",
+                    "write",
+                ]);
+            },
+            past() {
+                return randomElement([
+                    "ran",
+                    "died",
+                    "fell",
+                    "lied",
+                    "spoke",
+                    "thought",
+                    "talked",
+                    "beeped",
+                    "sang",
+                    "wrote",
+                ]);
+            },
+            future() {
+                const modifier = weightedRandomElement([
+                    [3, "will"],
+                    [1, "shall"],
+                ]);
+                return randomElement([
+                    `${modifier} run`,
+                    `${modifier} die`,
+                    `${modifier} fall`,
+                    `${modifier} lie`,
+                    `${modifier} speak`,
+                    `${modifier} think`,
+                    `${modifier} talk`,
+                    `${modifier} beep`,
+                    `${modifier} sing`,
+                    `${modifier} write`,
+                ]);
+            },
+        },
+    },
+    subjected: {
+        singular: {
+            anyTense() {
+                return randomElement([
+                    verbs.subjectless.singular.past,
+                    verbs.subjectless.singular.present,
+                    verbs.subjectless.singular.future,
+                ])();
+            },
+            present() {
+                return randomElement([
+                    "hits",
+                    "talks to",
+                    "considers",
+                    "speaks with",
+                    "kills",
+                    "eats",
+                    "licks",
+                    "converses with",
+                    "loves",
+                    "likes to eat",
+                ]);
+            },
+            past() {
+                return randomElement([
+                    "hit",
+                    "talked to",
+                    "considered",
+                    "spoke with",
+                    "killed",
+                    "ate",
+                    "lick",
+                    "conversed with",
+                    "loved",
+                ]);
+            },
+            future() {
+                const modifier = weightedRandomElement([
+                    [3, "will"],
+                    [1, "shall"],
+                ]);
+                return randomElement([
+                    `${modifier} hit`,
+                    `${modifier} talk to`,
+                    `${modifier} consider`,
+                    `${modifier} speak with`,
+                    `${modifier} kill`,
+                    `${modifier} eat`,
+                    `${modifier} lick`,
+                    `${modifier} converse with`,
+                    `${modifier} love`,
+                    `${modifier} like to eat`,
+                ]);
+            },
+        },
+        plural: {
+            anyTense() {
+                return randomElement([
+                    verbs.subjectless.singular.past,
+                    verbs.subjectless.singular.present,
+                    verbs.subjectless.singular.future,
+                ])();
+            },
+            present() {
+                return randomElement([
+                    "hit",
+                    "talk to",
+                    "consider",
+                    "speak with",
+                    "kill",
+                    "eat",
+                    "lick",
+                    "converse with",
+                    "love",
+                    "like to eat",
+                ]);
+            },
+            past() {
+                return randomElement([
+                    "hit",
+                    "talked to",
+                    "considered",
+                    "spoke with",
+                    "killed",
+                    "ate",
+                    "lick",
+                    "conversed with",
+                    "loved",
+                ]);
+            },
+            future() {
+                const modifier = weightedRandomElement([
+                    [3, "will"],
+                    [1, "shall"],
+                ]);
+                return randomElement([
+                    `${modifier} hit`,
+                    `${modifier} talk to`,
+                    `${modifier} consider`,
+                    `${modifier} speak with`,
+                    `${modifier} kill`,
+                    `${modifier} eat`,
+                    `${modifier} lick`,
+                    `${modifier} converse with`,
+                    `${modifier} love`,
+                    `${modifier} like to eat`,
+                ]);
+            },
+        },
     },
 };
 
@@ -223,8 +389,16 @@ function description() {
     ])();
 }
 
-function action() {
+function subjectlessAction() {
     return randomElement([
         () => `${articledThing()} ${verbs.subjectless.singular.anyTense()}`,
+        () => `${articledThings()} ${verbs.subjectless.plural.anyTense()}`,
+    ])();
+}
+
+function subjectedAction() {
+    return randomElement([
+        () => `${articledThing()} ${verbs.subjected.singular.anyTense()} ${randomElement([articledThings, articledThing])()}`,
+        () => `${articledThings()} ${verbs.subjected.plural.anyTense()} ${randomElement([articledThings, articledThing])()}`,
     ])();
 }
