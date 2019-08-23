68c2253101f8dcef0d45b80aff6e5388220de198
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-08-22 23:43:05 -0400

    Add alias for sentence

--- src/message-handlers/regular-interactions/decorative/sentence-generator/sentence.ts
+++ src/message-handlers/regular-interactions/decorative/sentence-generator/sentence.ts
@@ -4,7 +4,7 @@ import { randomSentence } from "./parts-of-speech";
 export function sentence(message: Message) {
     const { content } = message;
 
-    if (!content.match(/sentence/)) { return; }
+    if (!content.match(/sentence|speak/)) { return; }
 
     message.channel.send(randomSentence());
     return true;
