1bed828e5e5e406606f56e19e1dca0fb0358b60f
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-08-31 23:55:15 -0400

    Avoid starting discord on startup

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,15 @@ import { fs } from "mz";
 import { TypedJSON } from "typesafe-json";
 import { handleMessage } from "./message-handlers";
 
-const client = new Client();
-
-function onReady() {
-    console.log(`Logged in as ${client.user.tag}!`);
-}
-
 export async function setUp() {
-    client.on("ready", onReady);
+    const client = new Client();
+
+    client.on("ready", () => { console.log(`Logged in as ${client.user.tag}!`); });
     client.on("message", async (message) => { await handleMessage(client, message); });
-    await logIn();
+    await logIn(client);
 }
 
-async function logIn() {
+async function logIn(client: Client) {
     const tokenBuffer = (await fs.readFile("./secret.json"));
     const tokenJSON = TypedJSON.parse(tokenBuffer.toString());
     const token = tokenJSON.get("token").string();
