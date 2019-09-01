2cfa76882547ffb1431979f9a7c3da3da5841ae5
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2019-09-01 00:13:14 -0400

    Allow multiple sentences at once

--- src/message-handlers/regular-interactions/decorative/sentence-generator/sentence.ts
+++ src/message-handlers/regular-interactions/decorative/sentence-generator/sentence.ts
@@ -4,8 +4,17 @@ import { randomSentence } from "./parts-of-speech";
 export function sentence(message: Message) {
     const { content } = message;
 
-    if (!content.match(/sentence|speak/)) { return; }
+    const match = content.match(/sentence|speak(?: (\d+))?/);
 
-    message.channel.send(randomSentence());
+    if (!match) { return; }
+
+    let count = parseInt(match[1], 10) || 1;
+    if (count > 20) { count = 20; }
+
+    const sentences = [];
+    for (let i = 0; i < count; i++) { sentences.push(randomSentence()); }
+
+    message.channel.send(sentences.join("\n"));
+
     return true;
 }
