53914318fb3b92b07140256b6be5d26ed16e519d
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2020-11-23 00:07:41 -0500

    Limit deck dealing

--- src/message-handlers/regular-interactions/decorative/deck-dealer.ts
+++ src/message-handlers/regular-interactions/decorative/deck-dealer.ts
@@ -1,8 +1,13 @@
 import { Message } from "discord.js";
 import { delay } from "../../../utils/async";
 import { chance, randomElement, randomIn } from "../../../utils/random";
+import { RateLimiter } from "../../../utils/rate-limiter";
+
+const rateLimiter = new RateLimiter(1, 10 * 1000);
 
 export function deckDealer(message: Message) {
+    if (rateLimiter.hit("o")) { return; }
+
     if (!chance(0.1)) { return; }
 
     delay(randomIn(10000, 10000000)).then(async () => {
