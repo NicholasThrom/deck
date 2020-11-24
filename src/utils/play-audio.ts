f10ee834b23b235104c50d2deec503a470bd87c2
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2020-11-23 21:01:51 -0500

    Handle inaccessible channels

--- src/utils/play-audio.ts
+++ src/utils/play-audio.ts
@@ -1,18 +1,23 @@
 import { VoiceChannel, VoiceConnection } from "discord.js";
 import * as path from "path";
 
+export async function joinChannel(channel: VoiceChannel) {
+    try {
+        return await channel.join();
+    } catch (err) {
+        console.log(`Tried to join channel ${channel.name} in ${channel.guild.name} but did not have permission.`);
+    }
+}
+
 export async function playAudio(connection: VoiceConnection, audio: string) {
     return connection.play(path.join(__dirname, "..", "..", "..", "audio", audio));
 }
 
 export async function playAudioInChannel(channel: VoiceChannel, audio: string) {
-    try {
-        const connection = await channel.join();
-        const stream = await playAudio(connection, audio);
-        stream.on("finish", () => {
-            connection.disconnect();
-        });
-    } catch (err) {
-        console.log(`Tried to join channel ${channel.name} in ${channel.guild.name} but did not have permission.`);
-    }
+    const connection = await joinChannel(channel);
+    if (!connection) { return; }
+    const stream = await playAudio(connection, audio);
+    stream.on("finish", () => {
+        connection.disconnect();
+    });
 }
