7e1d3a6b5beb7e0f6cc3dc2c11d722b0460b26d1
Nicholas Throm <narthrom@edu.uwaterloo.ca>
2021-02-18 02:07:25 -0500

    Add wellerman

---
+++ src/message-handlers/regular-interactions/decorative/wellerman.ts
@@ -0,0 +1,22 @@
+import { Message, VoiceChannel } from "discord.js";
+import { playAudioInChannel } from "../../../utils/play-audio";
+
+export function decker(message: Message) {
+    const { content } = message;
+
+    if (!content.match(/wellerman/i)) { return; }
+    const channels = message.guild?.channels.cache?.values();
+    if (channels) {
+        for (const channel of Array.from(channels)) {
+            if (channel.type !== "voice") { continue; }
+            const voiceChannel = (channel as VoiceChannel)
+            const members = voiceChannel.members.values();
+            for (const member of Array.from(members)) {
+                if (member.id === message.author.id) {
+                    playAudioInChannel(voiceChannel, "odd/wellerman.mp3");
+                }
+            }
+        }
+    }
+    return true;
+}
